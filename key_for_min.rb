ca935c1dacb07e7eb65d97108992dc1df5c77316
Delia Constantino <dconstan92@gmail.com>
2020-10-30 18:58:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -34,45 +34,45 @@ end
 
 
 
-def key_for_min_value(hash)
-  # first we want to keep track of the lowest key
-  # and the lowest value
-  # so set two varaiables to keep track of both
-  # for example min_key = nil
-  min_key = ""
-  min_value = hash[min_key]
-
-  # then we want to iterate on the hash... preferably using .each
-  hash.each do |key, value|
-    # in here we want to do a type of comparison
-    if value <= min_value.to_i
-      min_key = key
-    end
-    # and update the min variables set above.
-  end
- min_key
-  # return min key down here
-end
-
-
-
-
-
-
-
-
-
-
 # def key_for_min_value(hash)
-# nums = []
-#   hash.collect do |name, num|
-#     nums << num
+#   # first we want to keep track of the lowest key
+#   # and the lowest value
+#   # so set two varaiables to keep track of both
+#   # for example min_key = nil
+#   min_key = ""
+#   min_value = hash[min_key]
+# 
+#   # then we want to iterate on the hash... preferably using .each
+#   hash.each do |key, value|
+#     # in here we want to do a type of comparison
+#     if value <= min_value.to_i
+#       min_key = key
+#     end
+#     # and update the min variables set above.
 #   end
-#   # hash.key(nums.min)
-#
-# new_num_array = []
-#   nums.each do |num|
-#
-#     if num < nums[0]
-#
+#  min_key
+#   # return min key down here
 # end
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # def key_for_min_value(hash)
+# # nums = []
+# #   hash.collect do |name, num|
+# #     nums << num
+# #   end
+# #   # hash.key(nums.min)
+# #
+# # new_num_array = []
+# #   nums.each do |num|
+# #
+# #     if num < nums[0]
+# #
+# # end
